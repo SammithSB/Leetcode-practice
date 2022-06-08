057c271778e9e3536b8f3274d66db7e0ae3b9d94
Sammith S Bharadwaj <54738235+SammithSB@users.noreply.github.com>
2022-06-08 14:18:36 +0530

    Time: 283 ms (66.32%), Space: 0B (100.00%) - LeetHub

--- 595-big-countries/595-big-countries.sql
+++ 595-big-countries/595-big-countries.sql
@@ -1,4 +1,8 @@
 # Write your MySQL query statement below
-select area, name, population from World where area >= 3000000 or population >= 25000000
+# select area, name, population from World where area >= 3000000 or population >= 25000000
 
 # another solutio would be to use Union
+
+select area, name, population from World where area >= 3000000 
+UNION 
+select area, name, population from World where population >= 25000000

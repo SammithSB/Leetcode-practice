336d22ba2bb460a49a50f86a0abb9eb7f6938fee
Sammith S Bharadwaj <54738235+SammithSB@users.noreply.github.com>
2022-06-10 13:52:01 +0530

    Time: 648 ms (38.81%), Space: 0B (100.00%) - LeetHub

--- 1873-calculate-special-bonus/1873-calculate-special-bonus.sql
+++ 1873-calculate-special-bonus/1873-calculate-special-bonus.sql
@@ -1,2 +1,4 @@
 # Write your MySQL query statement below
 select employee_id, if(employee_id%2!=0 and SUBSTRING(name, 1, 1)!='M',salary,0) as bonus from employees order by employee_id
+
+# if to check id and if first letter is M, order by clause to order values by employee id
